ec23deaa20cbf85c9cbaba147408df2cf1321bc3
Anant Janpage <anant.janpage4@gmail.com>
2022-05-27 03:40:33 +0530

    Updates

---
+++ precache-manifest.5a269e5d8c1569c5cfa1a830a6dcadd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10497c43183d276798",
+    "url": "/Online-foodcourt/static/css/main.3d00a349.chunk.css"
+  },
+  {
+    "revision": "ab10497c43183d276798",
+    "url": "/Online-foodcourt/static/js/main.d943a3fa.chunk.js"
+  },
+  {
+    "revision": "c27d684ea5ae7377b80f",
+    "url": "/Online-foodcourt/static/js/runtime~main.9334e9e2.js"
+  },
+  {
+    "revision": "7de05cdcf5c55c91ef70",
+    "url": "/Online-foodcourt/static/js/2.681bc38a.chunk.js"
+  },
+  {
+    "revision": "5cddbe2c43a78b7b48e582b49da7dd1c",
+    "url": "/Online-foodcourt/index.html"
+  }
+];
